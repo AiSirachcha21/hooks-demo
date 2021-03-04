15cfd522285b84a51c2385301b3b017383b286f5
Ryan Kuruppu <ryanjk.kuruppu@gmail.com>
2021-03-05 01:44:51 +0530

    Updated Navbar active state

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "./styles/navbar.css";
 
+const routes = ["/useState", "/useEffect", "/useContext"];
+
 function Navbar() {
+	const [activeLink, setActiveLink] = useState(0);
+	const location = useLocation();
+
+	useEffect(() => {
+		const route = routes.findIndex((r) => r === location.pathname);
+		setActiveLink(route);
+	}, [location.pathname]);
+
 	return (
 		<nav className="w-screen flex flex-row justify-between p-5 fixed bg-white">
 			<div>
@@ -9,13 +21,19 @@ function Navbar() {
 				</h3>
 			</div>
 			<div>
-				{["useState", "useEffect", "useContext"].map((item) => (
-					<a
-						className="text-blue-400 p-2 mx-2 transition duration-300 ease-in-out rounded-full hover:bg-blue-100"
-						href={`${item}`}
+				{routes.map((item, index) => (
+					<Link
+						key={index}
+						className={`text-blue-400 p-2 mx-2 transition duration-300 ease-in-out rounded-full hover:bg-blue-100 ${
+							index === activeLink && "bg-blue-200 rounded-lg"
+						}`}
+						to={item}
+						onClick={() => {
+							setActiveLink(index);
+						}}
 					>
-						{item}
-					</a>
+						{item.replace("/", "")}
+					</Link>
 				))}
 			</div>
 		</nav>
